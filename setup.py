896a4aeb83a8a12fd064964481ffca54340fa3f0
RGupta6586 <44025467+RGupta6586@users.noreply.github.com>
2018-10-11 12:46:14 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name="Test_Bot",
+    license="MIT",
+    version="1.0.0",
+    description="Sample program to test CI engines",
+    packages=["hello"],
+    classifiers=[
+        "Development Status :: 6 - Mature",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python"
+        ])
